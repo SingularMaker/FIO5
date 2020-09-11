36e6e9d7ddc9fe0243d59032504a7d9d33329a61
SingularBotTest <37448914+SingularMaker@users.noreply.github.com>
2020-09-11 23:45:21 +0800

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,21 +1,50 @@
 //% weight=0 color=#3CB371 icon="\uf1b9"
 namespace fio5 {
-	enum MotorLocation {
-		M1 = 0,//% p15,p16
-		M2 = 1,//% p14,p13
-		M3 = 2,//% p12,p11
-		M4 = 3//% p10,p09
+	export enum MotorLocation {
+		M1 = 0x01,//% p15,p16
+		M2 = 0x02,//% p14,p13
+		M3 = 0x03,//% p12,p11
+		M4 = 0x04//% p10,p09
 	}	
 
-	enum MotorDir {
-		CW = 0,//% p15,p16
-		CCW = 1//% p14,p13
+	export enum MotorDir {
+		CW = 0x01,//% p15,p16
+		CCW = 0x02//% p14,p13
 	}	
 	
-	/**
-    * singularbot board initialization, please execute at boot time
+    /** 
+	*馬達轉
 	*/
-	//% weight=100 blockId=singularbotInit block="Initialize singularbot"
-	export function singularbotInit() {
-	}	
+    //% blockId="motor_fn" block="Set Motor %position|pos %direction|dir %speed|Speed"
+    //% blockGap=1 weight=120
+    //% speed.min=0 speed.max=1023
+    export function motor_fn(pos: MotorLocation, dir: MotorDir, speed: number) {
+        let ctrl_0 = 0;
+		let ctrl_1 = 0;
+          		
+		if(dir == MotorDir.CW) {
+			ctrl_0 = speed;
+		} else {
+			ctrl_1 = speed;			
+		}
+		
+		if (pos == MotorLocation.M1 ) {
+          pins.analogWritePin(AnalogPin.P15, ctrl_0);
+          pins.analogWritePin(AnalogPin.P16, ctrl_1);
+		}
+		if ( pos == MotorLocation.M2 ) {
+          pins.analogWritePin(AnalogPin.P14, ctrl_0);
+          pins.analogWritePin(AnalogPin.P13, ctrl_1);			
+		}
+		
+		if ( pos == MotorLocation.M3 ) {
+          pins.analogWritePin(AnalogPin.P12, ctrl_0);
+          pins.analogWritePin(AnalogPin.P11, ctrl_1);			
+		}
+		
+		if ( pos == MotorLocation.M4 ) {
+          pins.analogWritePin(AnalogPin.P10, ctrl_0);
+          pins.analogWritePin(AnalogPin.P9, ctrl_1);		
+		}
+    }
 }
